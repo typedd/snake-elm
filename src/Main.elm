150de45fdc307316318270c0dcd86399168f0bc0
AVZapevalov <106755729+AVZapevalov@users.noreply.github.com>
2023-10-01 21:20:05 +0100

    reload clean

--- src/Main.elm
+++ src/Main.elm
@@ -8,7 +8,10 @@ import Element.Border as Border
 import Time
 import Keyboard exposing (RawKey)
 
+
+
 -- MAIN
+
 
 main =
   Browser.element 
@@ -17,7 +20,10 @@ main =
     , view = view
     , subscriptions = subscriptions }
 
+
+
 -- MODEL
+
 
 type alias Model =
   { headPosition : {x : Int, y : Int}
@@ -32,7 +38,10 @@ init _ =
   }, Cmd.none
   )
 
+
+
 -- UPDATE
+
 
 type DirSnake
     = UP
@@ -40,10 +49,12 @@ type DirSnake
     | LEFT
     | RIGHT
 
+
 type Msg
   = Tick Time.Posix
   | KeyDown RawKey
   | KeyUp RawKey
+
 
 update : Msg -> Model -> (Model, Cmd Msg)
 update msg model =
@@ -55,45 +66,59 @@ update msg model =
               UP ->
                 if model.headPosition.y <= 0 then {x = model.headPosition.x, y = 8}
                 else {x = model.headPosition.x, y = model.headPosition.y - 1}
+
               DOWN ->
                 if model.headPosition.y >= 8 then {x = model.headPosition.x, y = 0}
                 else {x = model.headPosition.x, y = model.headPosition.y + 1}
+
               LEFT ->
                 if model.headPosition.x <= 0 then {x = 8, y = model.headPosition.y}
                 else {x = model.headPosition.x - 1, y = model.headPosition.y}
+
               RIGHT ->
                 if model.headPosition.x >= 8 then {x = 0, y = model.headPosition.y}
                 else {x = model.headPosition.x + 1, y = model.headPosition.y}
+
         in
           ({model | headPosition = newHeadPosition}
           , Cmd.none
           )
+
       KeyDown key ->
         let
                 newDirection =
                     case Keyboard.rawValue key of
                       "ArrowUp" ->
                           UP
+
                       "ArrowDown" ->
                           DOWN
+
                       "ArrowLeft" ->
                           LEFT
+
                       "ArrowRight" ->
                           RIGHT
+
                       _ ->
                             model.directHead
             in
             (Debug.log (Debug.toString key)
               <|
                 { model | directHead = newDirection }, Cmd.none)
+
       KeyUp _ ->
         (model, Cmd.none)
 
+
+
 -- VIEW
+
 
 view : Model -> Html Msg
 view model =
   fieldDrow model.headPosition.x model.headPosition.y
+
 
 fieldDrow : Int -> Int -> Html msg
 fieldDrow xSnake ySnake =
@@ -104,35 +129,43 @@ fieldDrow xSnake ySnake =
       <|
         column [] (List.indexedMap (\yIndex _ -> fieldRow xSnake ySnake yIndex) (List.repeat 9 cell))
 
+
 fieldRow : Int -> Int -> Int -> Element msg
 fieldRow xSnake ySnake yIndex =
   Element.row [] (List.indexedMap (\xIndex _ -> pointSnake xSnake ySnake xIndex yIndex) (List.repeat 9 cell))
 
+
 pointSnake : Int -> Int -> Int -> Int -> Element msg
 pointSnake xSnake ySnake xIndex yIndex =
-  if (xSnake == xIndex && ySnake == yIndex ) then cellSnake else cell
+  if (xSnake == xIndex && ySnake == yIndex ) then cellSnake 
+  else cell
+
 
 cell : Element msg
 cell = 
   el
-        [ centerX, centerY,
-          Background.color (rgb255 240 0 245)
+      [ centerX, centerY,
+        Background.color (rgb255 240 0 245)
         , Border.rounded 3
         , padding 20
-        ]
-        Element.none
+      ]
+      Element.none
+
 
 cellSnake : Element msg
 cellSnake = 
   el
-        [ centerX, centerY,
-          Background.color (rgb255 0 250 0)
-        , Border.rounded 3
-        , padding 20
-        ]
-        Element.none
+      [ centerX, centerY,
+        Background.color (rgb255 0 250 0)
+      , Border.rounded 3
+      , padding 20
+      ]
+      Element.none
+
+
 
 -- SUBSCRIPTIONS
+
 
 subscriptions : Model -> Sub Msg
 subscriptions _ =
